3d1fc0ffa3271a3cbfb2bcfc61be6bfd2b104031
Thanh Thai <thanhthai@fulygon.com>
2024-04-21 11:58:28 +0700

    Update .histdb/zsh-history.db Update .local/share/bin/ffmpeg_transmitter.sh Update .local/share/zoxide/db.zo Update .zshrc Update .zsh_history

--- dot_local/share/bin/executable_ffmpeg_transmitter.sh
+++ dot_local/share/bin/executable_ffmpeg_transmitter.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+set -e
 
-sink_exists() {
-  pactl list sinks | grep -q "Sink Name: Remote"
+function cleanup {
+    pactl unload-module module-null-sink
 }
 
-if ! sink_exists; then
-  pactl load-module module-null-sink sink_name=Remote
+trap cleanup EXIT
+
+if [ -z "$1" ]
+then
+    echo "Please provide an IP address."
+    exit 1
 fi
+
+if ! pactl list short sinks | grep  "Remote"; then
+    pactl load-module module-null-sink sink_name=Remote
+fi
+
 
 ffmpeg -f pulse -i "Remote.monitor" -ac 2 -acodec pcm_s16le -ar 48000 -f u32le "udp://$1:18181"
