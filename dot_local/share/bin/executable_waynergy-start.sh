65066f1bd611ff9614e595ac1e98467ecb95d81e
Thanh Thai <thanhthai@fulygon.com>
2024-04-22 16:45:14 +0700

    Update .config/lazygit/state.yml Update .histdb/zsh-history.db Update .local/share/bin/waynergy-start.sh Update .zsh_history

--- dot_local/share/bin/executable_waynergy-start.sh
+++ dot_local/share/bin/executable_waynergy-start.sh
@@ -7,4 +7,5 @@ then
     exit 1
 fi
 
-nohup waynergy -En --backend kde --host $1 > ~/.config/waynergy/daemon.out 2> ~/.config/waynergy/daemon.err &
+# nohup waynergy -En --backend kde --host $1 > ~/.config/waynergy/daemon.out 2> ~/.config/waynergy/daemon.err &
+waynergy -En --backend kde --host $1
